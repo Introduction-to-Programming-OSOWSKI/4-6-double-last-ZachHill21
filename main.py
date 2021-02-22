124fb6dd59c4273df8fa1bee38ad935369f40416
ZachHill21 <814055@mystma.org>
2021-02-22 16:12:00 +0000

    try 2

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
 def doubleLast(j):
-    w= len(j)
-    j[len(j)-1]= w
-    j.append(w)
+    j.append(j[len(j)-1])
     return j
-print (doubleLast([1, 2, 3, 4, 5]))
+print (doubleLast([0,0,0,0,0,0,0,0,0,0,0,0,1]))
